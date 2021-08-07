515193ecb58b47f44aaf35ac8de02219f8964fac
MIGL <potato@potato>
2021-08-07 17:55:38 -0500

    first cheat commit

---
+++ ToIgnore.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package potato.gitumpproject001;
+
+/**
+ *
+ * @author mikhail
+ */
+public class ToIgnore {
+
+    private int potato = 0;
+}
